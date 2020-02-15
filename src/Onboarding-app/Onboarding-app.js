1d6627bdf476303db4543fb388bb92ca28b9b9d7
amitkumar <amitkumar392@gmail.com>
2020-02-15 17:28:51 +0530

    commit 1

--- src/Onboarding-app/Onboarding-app.js
+++ src/Onboarding-app/Onboarding-app.js
@@ -1,5 +1,6 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import "@fabricelements/skeleton-carousel/skeleton-carousel.js";
+import'./skeleton-carousel';
 /**
  * @customElement
  * @polymer
@@ -7,17 +8,52 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 class OnboardingApp extends PolymerElement {
   static get template() {
     return html`
-      <style>
+ 
+    <style>
         :host {
           display: block;
         }
+        #home{
+          height:100px;
+          width:100px;
+          background-color:pink;
+        }
       </style>
+  
+
+      <skeleton-page data-carousel={{dataCarousel}}>
+      
+      </skeleton-page>
+      
+
+
+    
 
     `;
   }
   static get properties() {
     return {
-     
+      dataCarousel: {
+        type: Array,
+        value: [
+          {
+            image: "../images/pic1.jpg",
+            title: "Title1"
+          },
+          {
+            image: "../images/pic2.jpg",
+            title: "Title2"
+          },
+          {
+            image: "../images/pic3.jpg",
+            title: "Title3"
+          },
+          {
+            image: "../images/pic4.jpg",
+            title: "Title4"
+          }
+        ]
+      }
     };
   }
 }
